9afc20a81367b59227953146792660596f854412
Masahiro Yagyu <gyu7372@gmail.com>
2017-10-13 14:53:45 +0900

    complete mooovi

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,16 @@
+class ProductsController < RankingController
+  before_action :authenticate_user!, only: :search
+
+  def index
+    @products = Product.order('id asc').limit(20)
+    binding.pry
+  end
+
+  def show
+    @product = Product.find params[:id]
+  end
+
+  def search
+    @products = Product.where('title LIKE(?)', "%#{params[:keyword]}%")
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :avatar,
+                    styles: { medium: "10x10#" }
+  validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
+
+  validates :nickname, presence: true
+  has_many :reviews
+end
